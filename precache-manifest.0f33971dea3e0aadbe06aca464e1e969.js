98c636fefb37d94ed6aaadfa95bbf58167884152
LIMJONGSOO <limjs4535@gmail.com>
2020-03-03 23:29:51 +0900

    Updates

---
+++ precache-manifest.0f33971dea3e0aadbe06aca464e1e969.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8996b16a544ae82667c14d13c3407a97",
+    "url": "/jongsoo/index.html"
+  },
+  {
+    "revision": "34b70b44d47c46925267",
+    "url": "/jongsoo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "538c8e561dd74649c8d8",
+    "url": "/jongsoo/static/js/2.749a6afa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jongsoo/static/js/2.749a6afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b70b44d47c46925267",
+    "url": "/jongsoo/static/js/main.fefd9909.chunk.js"
+  },
+  {
+    "revision": "f50ff6d391b05023db35",
+    "url": "/jongsoo/static/js/runtime-main.8548dbfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jongsoo/static/media/logo.5d5d9eef.svg"
+  }
+]);
